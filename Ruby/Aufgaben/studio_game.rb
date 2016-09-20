034ca4af760e491186058ce9808870c87ad5823c
Chriss122 <Christian_weikert@yahoo.com>
2016-09-20 10:49:17 +0200

    Aufgaben

--- Ruby/Aufgaben/studio_game.rb
+++ Ruby/Aufgaben/studio_game.rb
@@ -2,6 +2,6 @@ name1 = "larry\'s"
 name2 = "curly\'s"
 name3 = "moe\'s"
 health = 60
-puts "#{name1} health is #{health /9}  \n#{name2} health is #{health /9.0} \n#{name3} health is #{health /9} \n\tPlayers: \n\t\t#{name1}\n\t\t#{name2}\n\t\t#{name3}"
+puts "#{name1} health is #{(health /9).round(2)}  \n#{name2} health is #{(health /9.0).round(2)} \n#{name3} health is #{health /9} \n\tPlayers: \n\t\t#{name1}\n\t\t#{name2}\n\t\t#{name3}"
 
 
