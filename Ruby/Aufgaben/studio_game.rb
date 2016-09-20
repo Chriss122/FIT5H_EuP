7ed5a1a95a1567982caa4adb60b738d2981dfc62
Chriss122 <Christian_weikert@yahoo.com>
2016-09-20 11:04:34 +0200

    Aufgaben

--- Ruby/Aufgaben/studio_game.rb
+++ Ruby/Aufgaben/studio_game.rb
@@ -4,4 +4,10 @@ name3 = "moe\'s"
 health = 60
 puts "#{name1} health is #{(health /9).round(2)}  \n#{name2} health is #{(health /9.0).round(2)} \n#{name3} health is #{health /9} \n\tPlayers: \n\t\t#{name1}\n\t\t#{name2}\n\t\t#{name3}"
 
-
+allenamen = <<heredoc
+Players:
+	#{name1}
+		#{name2}
+			#{name3}
+heredoc
+puts allenamen
