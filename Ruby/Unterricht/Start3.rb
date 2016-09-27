60b42fb18c93dacabfde5fbe66c8ec133570a223
Chriss122 <Christian_weikert@yahoo.com>
2016-09-27 08:35:41 +0200

    Aufgaben

--- Ruby/Unterricht/Start3.rb
+++ Ruby/Unterricht/Start3.rb
@@ -1,10 +1,11 @@
-def movie_listing(movie, rank)#standart kann man setzen mit rank = 5zb in der Klammer
+def movie_listing(movie, rank)#standart kann man setzen mit rank = 5zb in der Klammer dann setzt er 5 wenn kein Wert gegeben ist.
 	"Movie: #{movie} ist ein guter Film, mit der Punktzahl: #{rank} am #{set_time}"
 end
 
 def set_time
  	Time.now.strftime("%d.%m.%Y")	
 end
+
 puts movie_listing("Tschick", 10)   #ARGV[0], ARGV[1] in die Klammern statt Tschick     ruby start.rb Superman 10
 
 
